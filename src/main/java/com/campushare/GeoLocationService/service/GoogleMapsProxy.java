ffc1099cd235bd173b7865853327c155cff4bfae
Aishwarya Singh <aishwaryasingh0602@gmail.com>
2023-11-28 10:43:33 -0800

    cache update and change server port

--- src/main/java/com/campushare/GeoLocationService/service/GoogleMapsProxy.java
+++ src/main/java/com/campushare/GeoLocationService/service/GoogleMapsProxy.java
@@ -4,6 +4,7 @@ import com.campushare.GeoLocationService.model.GeoLocationData;
 import com.campushare.GeoLocationService.model.Trip;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -13,12 +14,20 @@ public class GoogleMapsProxy implements GeoLocationService{
     private Map<Trip, GeoLocationData> geoLocationCache;
     private GoogleMapsService service;
 
+    private LocalDate lastRefresh;
+
     public GoogleMapsProxy(GoogleMapsService service) {
         this.service = service;
         this.geoLocationCache = new HashMap<>();
+        this.lastRefresh = LocalDate.now();
     }
     @Override
     public GeoLocationData getGeoLocationData(String origin, String stop, String destination) {
+        LocalDate today = LocalDate.now();
+        Boolean tooOld = this.lastRefresh.isBefore(today.minusDays(30));
+        if (tooOld) {
+            clearCache();
+        }
         Trip trip = new Trip(origin, stop, destination);
         if (this.geoLocationCache.containsKey(trip)) {
             System.out.println("Time found in cache!");
@@ -26,7 +35,9 @@ public class GoogleMapsProxy implements GeoLocationService{
         } else {
             System.out.println("Calling Google Maps API");
             GeoLocationData tripTime = this.service.getGeoLocationData(origin, stop, destination);
-            this.geoLocationCache.put(trip, tripTime);
+            if (tripTime.getAddedTime() < 10) {
+                this.geoLocationCache.put(trip, tripTime);
+            }
             return tripTime;
         }
 
@@ -34,6 +45,7 @@ public class GoogleMapsProxy implements GeoLocationService{
 
     public void clearCache() {
         this.geoLocationCache = new HashMap<>();
+        this.lastRefresh = LocalDate.now();
     }
 
     public int getCacheLength() {
